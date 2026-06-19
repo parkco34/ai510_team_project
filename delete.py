3280b91d9ea90f948482d6dc33b0322273774ffe
parkco34 <parkercorya@gmail.com>
2026-06-19 09:11:54 -0400

    deleted a a useless file

--- delete.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import pandas as pd
-
-df_hot = pd.read_csv("Hotel_Reviews.csv")
-df_clim = pd.read_csv("Worldwide Travel Cities Dataset (Ratings and Climate).csv")
-
-breakpoint()
-
